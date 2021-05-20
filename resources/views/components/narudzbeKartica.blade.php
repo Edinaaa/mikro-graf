ff8595974a35083be019a22343c84c6b358d83d7
Edinaaa <edina.ibrisimbegovic@edu.fit.ba>
2021-05-21 01:26:49 +0200

    narudzbe

--- resources/views/components/narudzbeKartica.blade.php
+++ resources/views/components/narudzbeKartica.blade.php
@@ -1,34 +1,15 @@
-<div class="min-h-screen bg-gray-200 flex justify-center items-center">
-   <!-- Start of component -->
-   <div class="max-w-xl m-2 bg-white border-2 border-gray-300 p-5 rounded-md tracking-wide shadow-lg">
-      <div id="header" class="flex"> 
-         <img alt="mountain" class="w-45 rounded-md border-2 border-gray-300" src="https://picsum.photos/seed/picsum/200" />
-         <div id="body" class="flex justify-between flex-col ml-5">
-            <div>
-               <h4 id="name" class="text-xl font-semibold mb-2">Plocica za vrata</h4>
-               <p id="job" class="text-gray-800 mt-2">Natpis, font, materijal, dimenzije, oblik, sav opis tekstualno.</p>
-            </div>
-            
-            <div class="flex justify-between mt-5">
-               <p class="">Isporuceno 1.1.2020 g</p>
-               <p class="text-primary-600 font-semibold text-lg">Cijena 20 KM</p>
 
-            </div>
-         </div>
-      </div>
-   </div>
-   <!-- End of component -->
      <!-- Start of component -->
-     <div class="max-w-xl m-2 lg:bg-gray-300  bg-white border-2 border-gray-300 p-5 rounded-md tracking-wide shadow-lg">
-      <div id="header" class="flex"> 
-         <img alt="mountain" class="w-45 rounded-md border-2 border-gray-300" src="https://picsum.photos/seed/picsum/200" />
+     <div class="max-w-xl m-2  bg-white border-2 border-gray-300 p-5 rounded-md tracking-wide shadow-lg">
+      <div id="header" class="flex flex-col xs:flex-row"> 
+         <img alt="mountain" class="w-45 h-1/2 rounded-md border-2 border-gray-300" src="https://picsum.photos/seed/picsum/200" />
          <div id="body" class="flex justify-between  flex-col ml-5">
             <div>
                <h4 id="naxiv" class="text-xl font-semibold mb-2">Plocica za vrata</h4>
                <p id="opis" class="text-gray-800 mt-2">Natpis, font, materijal, dimenzije, oblik, sav opis tekstualno.</p>
             </div>
             
-            <div class="flex  justify-between flex-col xl:flex-row mt-5">
+            <div class="flex  justify-between flex-row xs:flex-col sm:flex-row lg:flex-col xl:flex-row  mt-5">
                <p class="">Isporuceno 1.1.2020 g</p>
                <p class="text-primary-600 font-semibold text-lg">Cijena 20 KM</p>
 
@@ -37,4 +18,3 @@
       </div>
    </div>
    <!-- End of component -->
-</div>
--- resources/views/narudzba/narudzbe.blade.php
+++ resources/views/narudzba/narudzbe.blade.php
@@ -1,13 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
 
-<x-narudzbeKartica></x-narudzbeKartica>
-</div>
+    <div id="div" class="min-h-screen bg-gray-200 flex flex-col lg:grid lg:grid-cols-2 lg:gap-2 lg:justify-items-center justify-center items-center">
+   
+
+        <x-narudzbeKartica></x-narudzbeKartica>
+        <x-narudzbeKartica></x-narudzbeKartica>
+        <x-narudzbeKartica></x-narudzbeKartica>
+        <x-narudzbeKartica></x-narudzbeKartica>
+        <x-narudzbeKartica></x-narudzbeKartica>
+
+    </div>
 
 <script>
- 
+
 
 </script>
 @endsection
