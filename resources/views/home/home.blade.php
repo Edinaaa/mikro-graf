e666678ea863210316938b2326b000c6dcae8715
Edinaaa <edina.ibrisimbegovic@edu.fit.ba>
2021-08-02 12:54:48 +0200

    home responsive

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -2,19 +2,19 @@
 
 @section('content')
 <div class=" absolute top-0 flex-col">
-    <div class=" relative flex flex-row justify-items-center h-screen  w-full object-fill">
+    <div class=" relative flex flex-row justify-items-center h-1/3 md:h-screen  w-full object-fill">
         <img class=" shadow-2xl w-full" src="https://source.unsplash.com/random/1280x720" alt="">
         
-        <div class=" absolute flex flex-row w-full justify-center items-center bottom-10 text-5xl  text-primary-600">
+        <div class=" absolute flex flex-row w-full justify-center items-center bottom-10 text-2xl md:text-5xl  text-primary-600">
             <p class="flex justify-center">Dobro dosli</p>
         </div>
     </div>
 
-<div class="w-full flex-row md:flex items-center bg-gray-100 m-1 ">           
-    <div class=" w-1/2 flex p-1 m-1  lg:p-4 md:m-4">
+<div class="w-full flex flex-col md:flex-row  justify-center items-center bg-gray-100  ">           
+    <div class=" w-11/12 md:w-1/2 flex items-center  justify-center p-1 m-1 lg:p-4 md:m-4">
             <img class="rounded-lg shadow-md" src="https://source.unsplash.com/random/1280x720" alt="">
     </div>
-    <div class="w-1/2   flex flex-col items-center justify-center  p-1 m-1 lg:p-4 md:m-4 ">
+    <div class="w-11/12 md:w-1/2   flex flex-col items-center justify-center  p-1 m-1 lg:p-4 md:m-4 ">
         <p class="text-gray-900 font-semibold  text-4xl pl-3">Odaberite i narucite neki od nasih proizvoda.</p>
         <div class='flex items-center justify-center   py-5'>
         <a href="{{route('proizvodi')}}" class='w-auto bg-primary-600 hover:bg-primary-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Odaberi</a>
@@ -24,27 +24,29 @@
 
 </div>
 
-<div class="w-full flex-row md:flex items-center bg-gray-100 m-1 ">           
+
+
+<div class="w-full flex flex-col-reverse md:flex-row items-center bg-gray-100 m-1 ">           
     
-    <div class="w-1/2   flex flex-col items-center justify-center  p-1 m-1 lg:p-4 md:m-4 ">
+    <div class=" w-11/12 md:w-1/2   flex flex-col items-center justify-center  p-1 m-1 lg:p-4 md:m-4 ">
         <p class="text-gray-900 font-semibold  text-4xl pl-3">Pregledajte slike proizvoda koje mi nudimo.</p>
         <div class='flex items-center justify-center   py-5'>
         <a href="{{route('galerija')}}" class='w-auto bg-primary-600 hover:bg-primary-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Prelgedaj</a>
          </div>
 
     </div>
-    <div class=" w-1/2 flex p-1 m-1  lg:p-4 md:m-4">
+    <div class="  w-11/12 md:w-1/2 flex p-1 m-1  lg:p-4 md:m-4">
             <img class="rounded-lg shadow-md" src="https://source.unsplash.com/random/1280x720" alt="">
     </div>
 
 </div>
-<div class="w-full flex-row md:flex items-center bg-gray-100 m-1 ">           
-    <div class=" w-1/2 flex p-1 m-1  lg:p-4 md:m-4">
+<div class="w-full  flex flex-col md:flex-row  items-center bg-gray-100 m-1 ">           
+    <div class="  w-11/12 md:w-1/2  flex p-1 m-1  lg:p-4 md:m-4">
             <img class="rounded-lg shadow-md" src="https://source.unsplash.com/random/1280x720" alt="">
     </div>
-    <div class="w-1/2   flex flex-col items-center justify-center  p-1 m-1 lg:p-4 md:m-4 ">
+    <div class=" w-11/12 md:w-1/2    flex flex-col items-center justify-center  p-1 m-1 lg:p-4 md:m-4 ">
         <p class="text-gray-900 font-semibold  text-4xl pl-3">Zalite naruciti unikatan proizvod? </p>
-        <p class="text-gray-900 text-2xl">Ovdje mozete da navedete sve vase zahtjeve.</p>
+        <p class="text-gray-900 text-2xl pl-3 pt-1">Ovdje mozete da navedete sve vase zahtjeve.</p>
         <div class='flex items-center justify-center   py-5'>
         <a href="{{route('narudzba')}}" class='w-auto bg-primary-600 hover:bg-primary-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Naruci</a>
          </div>
@@ -53,26 +55,26 @@
 
 </div>
 
-<div class="w-full flex-row md:flex items-center bg-gray-100 m-1 ">           
+<div class="w-full flex flex-col-reverse md:flex-row items-center bg-gray-100 m-1 ">           
     
-    <div class="w-1/2   flex flex-col items-center justify-center  p-1 m-1 lg:p-4 md:m-4 ">
+    <div class=" w-11/12 md:w-1/2   flex flex-col items-center justify-center  p-1 m-1 lg:p-4 md:m-4 ">
         <p class="text-gray-900 font-semibold  text-4xl pl-3">Trebate vise informacija? Jevite nam se.</p>
         <div class='flex items-center justify-center   py-5'>
         <a href="{{route('kontakt')}}" class='w-auto bg-primary-600 hover:bg-primary-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Javi se</a>
          </div>
 
     </div>
-    <div class=" w-1/2 flex p-1 m-1  lg:p-4 md:m-4">
+    <div class="  w-11/12 md:w-1/2  flex p-1 m-1  lg:p-4 md:m-4">
             <img class="rounded-lg shadow-md" src="https://source.unsplash.com/random/1280x720" alt="">
     </div>
 
 </div>
 
-<div class="w-full flex-row md:flex items-center bg-gray-100 m-1 ">           
-    <div class=" w-1/2 flex p-1 m-1  lg:p-4 md:m-4">
+<div class="w-full  flex flex-col md:flex-row  items-center bg-gray-100 m-1 ">           
+    <div class=" w-11/12 md:w-1/2  flex p-1 m-1  lg:p-4 md:m-4">
             <img class="rounded-lg shadow-md" src="https://source.unsplash.com/random/1280x720" alt="">
     </div>
-    <div class="w-1/2   flex flex-col items-center justify-center  p-1 m-1 lg:p-4 md:m-4 ">
+    <div class=" w-11/12 md:w-1/2   flex flex-col items-center justify-center  p-1 m-1 lg:p-4 md:m-4 ">
         <p class="text-gray-900 font-semibold  text-4xl pl-3">Saznajte nesto vise o nama.</p>
         <div class='flex items-center justify-center   py-5'>
         <a href="{{route('onama')}}" class='w-auto bg-primary-600 hover:bg-primary-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>O nama</a>
@@ -82,16 +84,16 @@
 
 </div>
 
-<div class="w-full flex-row md:flex items-center bg-gray-100 m-1 ">           
+<div class="w-full flex flex-col-reverse md:flex-row items-center bg-gray-100 m-1 ">           
     
-    <div class="w-1/2   flex flex-col items-center justify-center  p-1 m-1 lg:p-4 md:m-4 ">
+    <div class=" w-11/12 md:w-1/2   flex flex-col items-center justify-center  p-1 m-1 lg:p-4 md:m-4 ">
         <p class="text-gray-900 font-semibold  text-4xl pl-3">Registrujte se kod nas, kako bi imali vise uvida u nasu suradnju.</p>
         <div class='flex items-center justify-center   py-5'>
         <a href="{{route('register')}}" class='w-auto bg-primary-600 hover:bg-primary-700 rounded-lg shadow-xl font-medium text-white px-4 py-2'>Registruj se</a>
          </div>
 
     </div>
-    <div class=" w-1/2 flex p-1 m-1  lg:p-4 md:m-4">
+    <div class="  w-11/12 md:w-1/2  flex p-1 m-1  lg:p-4 md:m-4">
             <img class="rounded-lg shadow-md" src="https://source.unsplash.com/random/1280x720" alt="">
     </div>
 
